b8abe7fd9fc0893718d3f693e4573f218c0b1efe
choisuchoi <nds200916@gmail.com>
2024-04-02 19:37:33 +0900

    fixed: Error(Elements two children...)

--- src/pages/specification/components/sepc-content/content-section/sepcSection2.js
+++ src/pages/specification/components/sepc-content/content-section/sepcSection2.js
@@ -10,22 +10,34 @@ function SepcSection2() {
             </div>
             <div className='table-section'>
                 <table>
-                    <tr><th>method</th><th>request URL</th></tr>
-                    <tr><td>POST</td><td>{serverURL}</td></tr>
+                    <thead>
+                        <tr><th>method</th><th>request URL</th></tr>
+                    </thead>
+                    <tbody>
+                        <tr><td>POST</td><td>{serverURL}</td></tr>
+                    </tbody>
                 </table>
             </div>
             <p className='title2'>Request Header</p>
             <div className='table-section'>
                 <table>
-                    <tr><th>parameter</th><th>type</th><th>essential</th><th>etc</th></tr>
-                    <tr><td>Content-Type</td><td>String</td><td>Yes</td><td>application/json</td></tr>
+                    <thead>
+                        <tr><th>parameter</th><th>type</th><th>essential</th><th>etc</th></tr>
+                    </thead>
+                    <tbody>
+                        <tr><td>Content-Type</td><td>String</td><td>Yes</td><td>application/json</td></tr>
+                    </tbody>
                 </table>
             </div>
             <p className='title2'>Request Elements</p>
             <div className='table-section'>
                 <table>
-                    <tr><th>parameter</th><th>type</th><th>essential</th><th>etc</th></tr>
-                    <tr><td>text</td><td>String</td><td>Yes</td><td>message</td></tr>
+                    <thead>
+                        <tr><th>parameter</th><th>type</th><th>essential</th><th>etc</th></tr>
+                    </thead>
+                    <tbody>
+                        <tr><td>text</td><td>String</td><td>Yes</td><td>message</td></tr>
+                    </tbody>
                 </table>
             </div>
         </div>
