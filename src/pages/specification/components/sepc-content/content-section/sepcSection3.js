f525fa1ec09e33eda7dc6024140d514866ed9c3b
choisuchoi <nds200916@gmail.com>
2024-04-02 23:44:32 +0900

    fixed: Top-button location

--- src/pages/specification/components/sepc-content/content-section/sepcSection3.js
+++ src/pages/specification/components/sepc-content/content-section/sepcSection3.js
@@ -1,13 +1,6 @@
-function SepcSection3() {
-    const responseSyntax =
-`[
-    "0.9997372031211853",
-    "4.3749314500018954e-05",
-    "9.505049092695117e-05",
-    "0.0001239777193404734",
-    "\\uc548\\ub155\\ud558\\uc138\\uc694"
-]`;
+import { responseSyntax } from "../../../specificationText";
 
+function SepcSection3() {
     return (
         <div className='content-section'>
             <p className='title1'>Response</p>
