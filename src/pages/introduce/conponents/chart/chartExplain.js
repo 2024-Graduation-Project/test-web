360514a12c973e874e582b20fa2300f5850d9abd
choisuchoi <nds200916@gmail.com>
2024-04-12 17:53:20 +0900

    style: Modify key-function, chart-container align

--- src/pages/introduce/conponents/chart/chartExplain.js
+++ src/pages/introduce/conponents/chart/chartExplain.js
@@ -1,8 +1,12 @@
+import scrollVisible from "../../../../custom-hook/introduce/scrollVisible";
+
 function ChartExplain() {
+    const isVisible = scrollVisible(1800);
+
     return (
         <div className='chart-explain-container'>
-            <div className='chart-explain'>
-                "The teenage gamers are being exposed to profanity."
+            <div className={`chart-explain ${isVisible ? 'visible' : ''}`}>
+                "The teenage gamers are being exposed to swear."
             </div>
         </div>
     );
--- src/pages/introduce/introduceText.js
+++ src/pages/introduce/introduceText.js
@@ -19,11 +19,11 @@ export const reasonsText = [
 export const ioText = {
     inputTextLv0: "안녕하세요",
     inputTextLv1: "이래서 대구애들은 안된다",
-    inputTextLv2: "이거 이상한 새끼네",
-    inputTextLv3: "야이 시발놈아",
+    inputTextLv2: "이거 이상한 새1끼네",
+    inputTextLv3: "야이 시@발놈아",
     outputTextLv0: "[lv0] 안녕하세요",
     outputTextLv1: "[lv1] 이래서 대구애들은 안된다",
-    outputTextLv2: "[lv2] 이거 이상한 OO네",
-    outputTextLv3: "[lv3] 야이 OOO아"
+    outputTextLv2: "[lv2] 이거 이상한 OOO네",
+    outputTextLv3: "[lv3] 야이 OOOO아"
 };
 
